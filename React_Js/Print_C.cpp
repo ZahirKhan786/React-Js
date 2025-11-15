d5315a8d2309efd7c451724c7c472c1b15167988
ZahirKhan786 <belimzahir78@gmail.com>
2025-11-15 10:03:40 +0530

    Auto commit from batch file

---
+++ React_Js/Print_C.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("Hello Word");
+	printf("Mangrol \n Nandrakhi\n");
+	int i=0;
+	for(i=0;i<=10;i++)
+	{
+		printf("%d\n",i);
+	}
+	return 0;
+}
